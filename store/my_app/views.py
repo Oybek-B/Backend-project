e12b5462eea3fc3bf1866e7f0b6eac77dc274494
Oybek-B <141325285+Oybek-B@users.noreply.github.com>
2023-09-10 21:28:33 +0300

    j

--- store/my_app/views.py
+++ store/my_app/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Hello world')
+
+
+
+def index(request):
+    return render(request, 'main/index.html')
